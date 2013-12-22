7304b0f5c7f27d5d5f4eed01bc55e6ef2c16c75b
faemino <faemino@gmail.com>
2013-12-22 21:22:00 +0100

    Añadido obtención de datos vía AJAX

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// http://stackoverflow.com/questions/11952485/jquery-mobile-trying-to-get-remote-content-with-ajax
+
+$(document).bind("mobileinit", function () {
+    $.mobile.allowCrossDomainPages = true;
+    $.support.cors = true;
+});
+
+$(document).bind("pagebeforechange", function (e, data) {
+    if (typeof data.toPage === "string") {
+        var u = $.mobile.path.parseUrl(data.toPage);
+        var re = /^#unidad/;
+        if (u.hash.search(re) !== -1) {
+            $.ajax({
+                url: "http://api.mobile",
+                datatype: "html",
+                success: function (data) {
+                    $('#unit-content').html(data);
+                    var math = document.getElementById("unit-content");
+                    MathJax.Hub.Queue(["Typeset",MathJax.Hub,math]);
+                },
+                error: function (e) {
+                    console.log(e);
+                }
+            });
+        }
+    }
+});
