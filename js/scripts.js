5ac50612d42f0b6365196d30a367a9cc053dd2b1
Israel <israel@sangakoo.com>
2013-12-27 12:55:51 +0100

    Añadidos los recursos gráficos (iconos y screens)

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,3 @@
-// http://stackoverflow.com/questions/11952485/jquery-mobile-trying-to-get-remote-content-with-ajax
-
 $(document).bind("mobileinit", function () {
     $.mobile.allowCrossDomainPages = true;
     $.support.cors = true;
@@ -26,27 +24,4 @@ $(document).ready(function() {
             });
         }
     });
-
-/*
-    $(document).bind("pagebeforechange", function (e, data) {
-        alert(unit);
-        if (typeof data.toPage === "string") {
-            var u = $.mobile.path.parseUrl(data.toPage);
-            var re = /^#unidad/;
-            if (u.hash.search(re) !== -1) {
-                $.ajax({
-                    url: "http://api.mobile",
-                    datatype: "html",
-                    success: function (data) {
-                        $('#unit-content').html(data);
-                        var math = document.getElementById("unit-content");
-                        MathJax.Hub.Queue(["Typeset",MathJax.Hub,math]);
-                    },
-                    error: function (e) {
-                        console.log(e);
-                    }
-                });
-            }
-        }
-    });*/
 });
